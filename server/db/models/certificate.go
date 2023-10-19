cf7a9589ea34ec3b7a841373526264a96309d1b9
huylllc <632781087@qq.com>
2023-10-19 15:47:17 +0800

    add ca model

---
+++ server/db/models/certificate.go
@@ -0,0 +1,28 @@
+package models
+
+import (
+	"github.com/gofrs/uuid"
+	"gorm.io/gorm"
+	"time"
+)
+
+// Certificate - Certificate database model
+type Certificate struct {
+	ID             uuid.UUID `gorm:"primaryKey;->;<-:create;type:uuid;"`
+	CreatedAt      time.Time `gorm:"->;<-:create;"`
+	CommonName     string
+	CAType         string
+	KeyType        string
+	CertificatePEM string
+	PrivateKeyPEM  string
+}
+
+// BeforeCreate - GORM hook to automatically set values
+func (c *Certificate) BeforeCreate(tx *gorm.DB) (err error) {
+	c.ID, err = uuid.NewV4()
+	if err != nil {
+		return err
+	}
+	c.CreatedAt = time.Now()
+	return nil
+}
