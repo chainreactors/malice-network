1a4d13dda3673d2e9310322475b70fcb11afb299
M09Ic <m09ician@gmail.com>
2024-01-17 04:13:20 +0800

    fix implant register bug

--- server/rpc/rpc-implant.go
+++ server/rpc/rpc-implant.go
@@ -2,7 +2,6 @@ package rpc
 
 import (
 	"context"
-	"fmt"
 	"github.com/chainreactors/logs"
 	"github.com/chainreactors/malice-network/proto/implant/commonpb"
 	"github.com/chainreactors/malice-network/proto/listener/lispb"
@@ -15,17 +14,16 @@ func (rpc *Server) Register(ctx context.Context, req *lispb.RegisterSession) (*c
 	sess := core.NewSession(req)
 	core.Sessions.Add(sess)
 	dbSession := db.Session()
-	err := dbSession.Create(models.ConvertToSessionDB(sess)).Error
-	newErr := fmt.Errorf("%w", db.ERRNOEXISTS)
-	if err.Error() != newErr.Error() {
-		logs.Log.Errorf("cannot create session %s , %s in db", sess.ID, err.Error())
-		return nil, err
+	d := dbSession.Create(models.ConvertToSessionDB(sess))
+	if d.Error != nil {
+		logs.Log.Warnf("session %s re-register ", sess.ID)
+		return &commonpb.Empty{}, nil
 	}
 	logs.Log.Importantf("init new session %s from %s", sess.ID, sess.ListenerId)
 	return &commonpb.Empty{}, nil
 }
 
 func (rpc *Server) Ping(ctx context.Context, req *commonpb.Ping) (*commonpb.Empty, error) {
-	fmt.Println(req)
+	//fmt.Println(req)
 	return &commonpb.Empty{}, nil
 }
