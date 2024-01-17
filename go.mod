0f7346959a7a338cdb50e9603fa3e568eb54d02d
M09Ic <m09ician@gmail.com>
2024-01-17 16:30:56 +0800

    add sgn,donut,srdi generate

--- go.mod
+++ go.mod
@@ -14,9 +14,11 @@ require (
 	github.com/jessevdk/go-flags v1.5.0
 	github.com/klauspost/compress v1.17.0
 	github.com/ncruces/go-sqlite3 v0.9.0
+	github.com/pkg/errors v0.8.1
 	github.com/pterm/pterm v0.12.69
 	github.com/tetratelabs/wazero v1.5.0
 	golang.org/x/crypto v0.15.0
+	golang.org/x/net v0.18.0
 	golang.org/x/term v0.14.0
 	google.golang.org/grpc v1.59.0
 	google.golang.org/protobuf v1.31.0
@@ -28,9 +30,10 @@ require (
 	atomicgo.dev/cursor v0.2.0 // indirect
 	atomicgo.dev/keyboard v0.2.9 // indirect
 	atomicgo.dev/schedule v0.1.0 // indirect
-	github.com/atotto/clipboard v0.1.4 // indirect
+	github.com/Binject/debug v0.0.0-20210312092933-6277045c2fdf // indirect
+	github.com/Binject/go-donut v0.0.0-20220908180326-fcdcc35d591c // indirect
+	github.com/akamensky/argparse v1.3.0 // indirect
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
-	github.com/calyptia/go-bubble-table v0.2.1 // indirect
 	github.com/chainreactors/grumble v0.0.0-20240111103643-830a8bf7d70a // indirect
 	github.com/charmbracelet/bubbles v0.17.1 // indirect
 	github.com/charmbracelet/bubbletea v0.25.0 // indirect
@@ -41,9 +44,8 @@ require (
 	github.com/desertbit/columnize v2.1.0+incompatible // indirect
 	github.com/desertbit/go-shlex v0.1.1 // indirect
 	github.com/desertbit/readline v1.5.1 // indirect
-	github.com/erikgeiser/promptkit v0.9.0 // indirect
-	github.com/evertras/bubble-table v0.15.6 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/uuid v1.3.1 // indirect
 	github.com/gookit/color v1.5.4 // indirect
 	github.com/gookit/goutil v0.6.14 // indirect
 	github.com/grpc-ecosystem/go-grpc-middleware v1.4.0 // indirect
@@ -53,17 +55,15 @@ require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	github.com/juju/ansiterm v0.0.0-20210929141451-8b71cc96ebdc // indirect
 	github.com/kr/pretty v0.3.1 // indirect
 	github.com/lithammer/fuzzysearch v1.1.8 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
-	github.com/lunixbochs/vtclean v1.0.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-localereader v0.0.1 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
-	github.com/muesli/ansi v0.0.0-20230316100256-276c6243b2f6 // indirect
+	github.com/muesli/ansi v0.0.0-20211018074035-2e021307bc4b // indirect
 	github.com/muesli/cancelreader v0.2.2 // indirect
 	github.com/muesli/reflow v0.3.0 // indirect
 	github.com/muesli/termenv v0.15.2 // indirect
@@ -71,7 +71,6 @@ require (
 	github.com/rivo/uniseg v0.4.4 // indirect
 	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
 	golang.org/x/exp v0.0.0-20231127185646-65229373498e // indirect
-	golang.org/x/net v0.14.0 // indirect
 	golang.org/x/sync v0.5.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
---
+++ server/internal/generate/srdi.go
@@ -0,0 +1,270 @@
+package generate
+
+import (
+	"encoding/binary"
+	"math"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+)
+
+// ShellcodeRDIToFile generates a sRDI shellcode and writes it to a file
+func ShellcodeRDIToFile(dllPath string, functionName string) (shellcodePath string, err error) {
+	shellcode, err := ShellcodeRDI(dllPath, functionName, "")
+	if err != nil {
+		return "", err
+	}
+	dir := filepath.Dir(dllPath)
+	filename := strings.Replace(path.Base(dllPath), ".dll", ".bin", 1)
+	filePath := filepath.Join(dir, filename)
+	os.WriteFile(filePath, shellcode, 0700)
+	return filePath, nil
+}
+
+// ShellcodeRDI generates a reflective shellcode based on a DLL file
+func ShellcodeRDI(dllPath string, functionName string, userdata string) (shellcode []byte, err error) {
+	// handle command line arguments, -h or -help shows the menu
+	userDataStr := userdata
+	clearHeader := true
+
+	dllBytes, err := os.ReadFile(dllPath)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	// functionHash is 0x10 by default, otherwise get the hash and convert to bytes
+	var hashFunction []byte
+	if functionName != "" {
+		hashFunctionUint32 := hashFunctionName(functionName)
+		hashFunction = pack(hashFunctionUint32)
+	} else {
+		hashFunction = pack(uint32(0x10))
+	}
+
+	flags := 0
+	if clearHeader {
+		flags |= 0x1
+	}
+	var userData []byte
+	if userDataStr != "" {
+		userData = []byte(userDataStr)
+	}
+	shellcode = convertToShellcode(dllBytes, hashFunction, userData, flags)
+	//	err = os.RemoveAll(path.Clean(path.Dir(dllPath) + "/../"))
+	return shellcode, nil
+
+}
+
+// ShellcodeRDIFromBytes generate a sRDI from a byte array
+func ShellcodeRDIFromBytes(data []byte, functionName string, arguments string) (shellcode []byte, err error) {
+
+	clearHeader := true
+	userDataStr := arguments
+
+	// functionHash is 0x10 by default, otherwise get the hash and convert to bytes
+	var hashFunction []byte
+	if functionName != "" {
+		hashFunctionUint32 := hashFunctionName(functionName)
+		hashFunction = pack(hashFunctionUint32)
+	} else {
+		hashFunction = pack(uint32(0x10))
+	}
+
+	flags := 0
+	if clearHeader {
+		flags |= 0x1
+	}
+	var userData []byte
+	if userDataStr != "" {
+		userData = []byte(userDataStr)
+	}
+	shellcode = convertToShellcode(data, hashFunction, userData, flags)
+	return shellcode, nil
+}
+
+func convertToShellcode(dllBytes, functionHash, userData []byte, flags int) []byte {
+
+	if userData == nil {
+		userData = []byte("None")
+	}
+
+	var final []byte
+
+	if is64BitDLL(dllBytes) {
+		// do 64 bit things
+
+		bootstrapSize := 64
+
+		// call next instruction (Pushes next instruction address to stack)
+		bootstrap := []byte{0xe8, 0x00, 0x00, 0x00, 0x00}
+
+		// Set the offset to our DLL from pop result
+		dllOffset := bootstrapSize - len(bootstrap) + len(rdiShellcode64)
+
+		// pop rcx - Capture our current location in memory
+		bootstrap = append(bootstrap, 0x59)
+
+		// mov r8, rcx - copy our location in memory to r8 before we start modifying RCX
+		bootstrap = append(bootstrap, 0x49, 0x89, 0xc8)
+
+		// add rcx, <Offset of the DLL>
+		bootstrap = append(bootstrap, 0x48, 0x81, 0xc1)
+
+		bootstrap = append(bootstrap, pack(uint32(dllOffset))...)
+
+		// mov edx, <Hash of function>
+		bootstrap = append(bootstrap, 0xba)
+		bootstrap = append(bootstrap, functionHash...)
+
+		// Setup the location of our user data
+		// add r8, <Offset of the DLL> + <Length of DLL>
+		bootstrap = append(bootstrap, 0x49, 0x81, 0xc0)
+		userDataLocation := dllOffset + len(dllBytes)
+		bootstrap = append(bootstrap, pack(uint32(userDataLocation))...)
+
+		// mov r9d, <Length of User Data>
+		bootstrap = append(bootstrap, 0x41, 0xb9)
+		bootstrap = append(bootstrap, pack(uint32(len(userData)))...)
+
+		// push rsi - save original value
+		bootstrap = append(bootstrap, 0x56)
+
+		// mov rsi, rsp - store our current stack pointer for later
+		bootstrap = append(bootstrap, 0x48, 0x89, 0xe6)
+
+		// and rsp, 0x0FFFFFFFFFFFFFFF0 - Align the stack to 16 bytes
+		bootstrap = append(bootstrap, 0x48, 0x83, 0xe4, 0xf0)
+
+		// sub rsp, 0x30 - Create some breathing room on the stack
+		bootstrap = append(bootstrap, 0x48, 0x83, 0xec)
+		bootstrap = append(bootstrap, 0x30) // 32 bytes for shadow space + 8 bytes for last arg + 8 bytes for stack alignment
+
+		// mov dword ptr [rsp + 0x20], <Flags> - Push arg 5 just above shadow space
+		bootstrap = append(bootstrap, 0xC7, 0x44, 0x24)
+		bootstrap = append(bootstrap, 0x20)
+		bootstrap = append(bootstrap, pack(uint32(flags))...)
+
+		// call - Transfer execution to the RDI
+		bootstrap = append(bootstrap, 0xe8)
+		bootstrap = append(bootstrap, byte(bootstrapSize-len(bootstrap)-4)) // Skip over the remainder of instructions
+		bootstrap = append(bootstrap, 0x00, 0x00, 0x00)
+
+		// mov rsp, rsi - Reset our original stack pointer
+		bootstrap = append(bootstrap, 0x48, 0x89, 0xf4)
+
+		// pop rsi - Put things back where we left them
+		bootstrap = append(bootstrap, 0x5e)
+
+		// ret - return to caller
+		bootstrap = append(bootstrap, 0xc3)
+
+		final = append(bootstrap, rdiShellcode64...)
+		final = append(final, dllBytes...)
+		final = append(final, userData...)
+
+	} else {
+		// do 32 bit things
+
+		bootstrapSize := 45
+
+		// call next instruction (Pushes next instruction address to stack)
+		bootstrap := []byte{0xe8, 0x00, 0x00, 0x00, 0x00}
+
+		// Set the offset to our DLL from pop result
+		dllOffset := bootstrapSize - len(bootstrap) + len(rdiShellcode32)
+
+		// pop eax - Capture our current location in memory
+		bootstrap = append(bootstrap, 0x58)
+
+		// mov ebx, eax - copy our location in memory to ebx before we start modifying eax
+		bootstrap = append(bootstrap, 0x89, 0xc3)
+
+		// add eax, <Offset to the DLL>
+		bootstrap = append(bootstrap, 0x05)
+		bootstrap = append(bootstrap, pack(uint32(dllOffset))...)
+
+		// add ebx, <Offset to the DLL> + <Size of DLL>
+		bootstrap = append(bootstrap, 0x81, 0xc3)
+		userDataLocation := dllOffset + len(dllBytes)
+		bootstrap = append(bootstrap, pack(uint32(userDataLocation))...)
+
+		// push <Flags>
+		bootstrap = append(bootstrap, 0x68)
+		bootstrap = append(bootstrap, pack(uint32(flags))...)
+
+		// push <Length of User Data>
+		bootstrap = append(bootstrap, 0x68)
+		bootstrap = append(bootstrap, pack(uint32(len(userData)))...)
+
+		// push ebx
+		bootstrap = append(bootstrap, 0x53)
+
+		// push <hash of function>
+		bootstrap = append(bootstrap, 0x68)
+		bootstrap = append(bootstrap, functionHash...)
+
+		// push eax
+		bootstrap = append(bootstrap, 0x50)
+
+		// call - Transfer execution to the RDI
+		bootstrap = append(bootstrap, 0xe8)
+		bootstrap = append(bootstrap, byte(bootstrapSize-len(bootstrap)-4)) // Skip over the remainder of instructions
+		bootstrap = append(bootstrap, 0x00, 0x00, 0x00)
+
+		// add esp, 0x14 - correct the stack pointer
+		bootstrap = append(bootstrap, 0x83, 0xc4, 0x14)
+
+		// ret - return to caller
+		bootstrap = append(bootstrap, 0xc3)
+
+		final = append(bootstrap, rdiShellcode32...)
+		final = append(final, dllBytes...)
+		final = append(final, userData...)
+	}
+
+	return final
+
+}
+
+// helper function similar to struct.pack from python3
+func pack(val uint32) []byte {
+	bytes := make([]byte, 4)
+	binary.LittleEndian.PutUint32(bytes, val)
+	return bytes
+}
+
+// "HelloWorld" = 3571859646
+func hashFunctionName(name string) uint32 {
+	function := []byte(name)
+	function = append(function, 0x00)
+
+	functionHash := uint32(0)
+
+	for _, b := range function {
+		functionHash = ror(functionHash, 13, 32)
+		functionHash += uint32(b)
+	}
+
+	return functionHash
+}
+
+// ROR-13 implementation
+func ror(val uint32, rBits uint32, maxBits uint32) uint32 {
+	exp := uint32(math.Exp2(float64(maxBits))) - 1
+	return ((val & exp) >> (rBits % maxBits)) | (val << (maxBits - (rBits % maxBits)) & exp)
+}
+
+func is64BitDLL(dllBytes []byte) bool {
+	machineIA64 := uint16(512)
+	machineAMD64 := uint16(34404)
+
+	headerOffset := binary.LittleEndian.Uint32(dllBytes[60:64])
+	machine := binary.LittleEndian.Uint16(dllBytes[headerOffset+4 : headerOffset+4+2])
+
+	// 64 bit
+	if machine == machineIA64 || machine == machineAMD64 {
+		return true
+	}
+	return false
+}
