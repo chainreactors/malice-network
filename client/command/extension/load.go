f752d79521381165df1c50c1fa67637b2b5552e7
M09Ic <m09ician@gmail.com>
2024-01-17 14:52:11 +0800

    index on dev: b3eb82f remove helper depend client

---
+++ client/command/extension/load.go
@@ -0,0 +1,41 @@
+package extension
+
+const (
+	defaultTimeout = 60
+
+	// ManifestFileName - Extension manifest file name
+	ManifestFileName = "extension.json"
+)
+
+var loadedExtensions = map[string]*ExtensionManifest{}
+
+type ExtensionManifest struct {
+	Name            string               `json:"name"`
+	CommandName     string               `json:"command_name"`
+	Version         string               `json:"version"`
+	ExtensionAuthor string               `json:"extension_author"`
+	OriginalAuthor  string               `json:"original_author"`
+	RepoURL         string               `json:"repo_url"`
+	Help            string               `json:"help"`
+	LongHelp        string               `json:"long_help"`
+	Files           []*extensionFile     `json:"files"`
+	Arguments       []*extensionArgument `json:"arguments"`
+	Entrypoint      string               `json:"entrypoint"`
+	DependsOn       string               `json:"depends_on"`
+	Init            string               `json:"init"`
+
+	RootPath string `json:"-"`
+}
+
+type extensionFile struct {
+	OS   string `json:"os"`
+	Arch string `json:"arch"`
+	Path string `json:"path"`
+}
+
+type extensionArgument struct {
+	Name     string `json:"name"`
+	Type     string `json:"type"`
+	Desc     string `json:"desc"`
+	Optional bool   `json:"optional"`
+}
