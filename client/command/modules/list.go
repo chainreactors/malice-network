0c288330d3dd1d2d750b07d81a9ac918be905717
HuYlllc <632781087@qq.com>
2024-08-14 03:26:35 +0800

    Fix: Use table in list_module

--- client/command/modules/list.go
+++ client/command/modules/list.go
@@ -1,12 +1,14 @@
 package modules
 
 import (
+	"fmt"
 	"github.com/chainreactors/grumble"
 	"github.com/chainreactors/malice-network/client/console"
 	"github.com/chainreactors/malice-network/proto/implant/implantpb"
 	"github.com/chainreactors/tui"
-	"github.com/charmbracelet/bubbles/list"
+	"github.com/charmbracelet/bubbles/table"
 	"google.golang.org/protobuf/proto"
+	"os"
 )
 
 func listModules(ctx *grumble.Context, con *console.Console) {
@@ -14,27 +16,28 @@ func listModules(ctx *grumble.Context, con *console.Console) {
 	if session == nil {
 		return
 	}
-	sid := con.ActiveTarget.GetInteractive().SessionId
 	listTask, err := con.Rpc.ListModules(con.ActiveTarget.Context(), &implantpb.Empty{})
 	if err != nil {
 		console.Log.Errorf("ListModules error: %v", err)
 		return
 	}
-	resultChan := make(chan *implantpb.Modules)
 	con.AddCallback(listTask.TaskId, func(msg proto.Message) {
 		resp := msg.(*implantpb.Spite).GetModules()
-		resultChan <- resp
+		var rowEntries []table.Row
+		var row table.Row
+		tableModel := tui.NewTable([]table.Column{
+			{Title: "Name", Width: 15},
+			{Title: "Help", Width: 30},
+		}, true)
+		for _, module := range resp.GetModules() {
+			row = table.Row{
+				module,
+				"",
+			}
+			rowEntries = append(rowEntries, row)
+		}
+		tableModel.SetRows(rowEntries)
+		fmt.Printf(tableModel.View(), os.Stdout)
 	})
-	result := <-resultChan
-	var modules = make([]list.Item, 0)
-	for _, module := range result.GetModules() {
-		modules = append(modules, tui.Item{Ititle: module, Desc: ""})
-	}
-	listModel := tui.Newlist(modules)
-	newList := tui.NewModel(listModel, nil, false, false)
-	err = newList.Run()
-	if err != nil {
-		con.SessionLog(sid).Errorf("Error running list: %v", err)
-		return
-	}
+
 }
