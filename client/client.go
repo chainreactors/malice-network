c77b868502d120645f7e98e9d351ff4838eb89aa
M09Ic <m09ician@gmail.com>
2024-08-16 21:34:09 +0800

    fix github action compile

--- client/client.go
+++ client/client.go
@@ -1,11 +1,11 @@
 package main
 
-//go:generate protoc -I proto/ proto/client/clientpb/client.proto --go_out=paths=source_relative:proto/
-//go:generate protoc -I proto/ proto/client/rootpb/root.proto --go_out=paths=source_relative:proto/
-//go:generate protoc -I proto/ proto/implant/implantpb/implant.proto --go_out=paths=source_relative:proto/
-//go:generate protoc -I proto/ proto/listener/lispb/listener.proto --go_out=paths=source_relative:proto/
-//go:generate protoc -I proto/ proto/services/clientrpc/service.proto --go_out=paths=source_relative:proto/ --go-grpc_out=paths=source_relative:proto/
-//go:generate protoc -I proto/ proto/services/listenerrpc/service.proto --go_out=paths=source_relative:proto/ --go-grpc_out=paths=source_relative:proto/
+//go:generate protoc -I ../proto/ ../proto/client/clientpb/client.proto --go_out=paths=source_relative:../proto/
+//go:generate protoc -I ../proto/ ../proto/client/rootpb/root.proto --go_out=paths=source_relative:../proto/
+//go:generate protoc -I ../proto/ ../proto/implant/implantpb/implant.proto --go_out=paths=source_relative:../proto/
+//go:generate protoc -I ../proto/ ../proto/listener/lispb/listener.proto --go_out=paths=source_relative:../proto/
+//go:generate protoc -I ../proto/ ../proto/services/clientrpc/service.proto --go_out=paths=source_relative:../proto/ --go-grpc_out=paths=source_relative:../proto/
+//go:generate protoc -I ../proto/ ../proto/services/listenerrpc/service.proto --go_out=paths=source_relative:../proto/ --go-grpc_out=paths=source_relative:../proto/
 
 import (
 	"github.com/chainreactors/logs"
