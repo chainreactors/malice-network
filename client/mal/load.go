c026da5e705d5512582a555ef63b816add3767c1
M09Ic <m09ician@gmail.com>
2024-08-30 18:52:57 +0800

    enhance: mal load and cmd bind

--- client/mal/load.go
+++ client/mal/load.go
@@ -3,6 +3,7 @@ package mal
 import (
 	"github.com/chainreactors/malice-network/client/assets"
 	"github.com/chainreactors/malice-network/client/console"
+	"github.com/chainreactors/malice-network/client/core/plugin"
 	"github.com/spf13/cobra"
 	"os"
 	"path/filepath"
@@ -10,19 +11,25 @@ import (
 
 func MalLoadCmd(ctx *cobra.Command, con *console.Console) {
 	dirPath := ctx.Flags().Arg(0)
-	content, err := os.ReadFile(filepath.Join(assets.GetMalsDir(), dirPath, ManifestFileName))
+	_, err := LoadMalManiFest(con, dirPath)
 	if err != nil {
-		console.Log.Errorf(err.Error())
-		return
+		console.Log.Error(err)
+	}
+}
+
+func LoadMalManiFest(con *console.Console, filename string) (*plugin.MalManiFest, error) {
+	content, err := os.ReadFile(filepath.Join(assets.GetMalsDir(), filename, ManifestFileName))
+	if err != nil {
+		return nil, err
 	}
 	manifest, err := ParseMalManifest(content)
 	if err != nil {
-		return
+		return nil, err
 	}
 
 	err = con.Plugins.LoadPlugin(manifest, con)
 	if err != nil {
-		console.Log.Errorf(err.Error())
-		return
+		return nil, err
 	}
+	return manifest, nil
 }
