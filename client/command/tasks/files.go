14776fd4285d2fbcb6bb18831aeae3537bf40552
HuYlllc <632781087@qq.com>
2024-09-20 22:56:26 +0800

    Fix: Rename unsetEnv and add file id

--- client/command/tasks/files.go
+++ client/command/tasks/files.go
@@ -1,6 +1,7 @@
 package tasks
 
 import (
+	"fmt"
 	"github.com/chainreactors/malice-network/client/repl"
 	"github.com/chainreactors/malice-network/proto/client/clientpb"
 	"github.com/chainreactors/tui"
@@ -26,14 +27,16 @@ func printFiles(files *clientpb.Files, con *repl.Console) {
 	var rowEntries []table.Row
 	var row table.Row
 	tableModel := tui.NewTable([]table.Column{
+		{Title: "FileID", Width: 8},
 		{Title: "Name", Width: 20},
-		{Title: "TempID", Width: 15},
+		{Title: "TempID", Width: 10},
 		{Title: "Type", Width: 10},
 		{Title: "LocalName", Width: 30},
 		{Title: "RemotePath", Width: 30},
 	}, true)
 	for _, file := range files.Files {
 		row = table.Row{
+			file.TaskId,
 			file.Name,
 			file.TempId,
 			file.Op,
@@ -43,9 +46,10 @@ func printFiles(files *clientpb.Files, con *repl.Console) {
 		rowEntries = append(rowEntries, row)
 	}
 	tableModel.SetRows(rowEntries)
-	newTable := tui.NewModel(tableModel, nil, false, false)
-	err := newTable.Run()
-	if err != nil {
-		con.Log.Errorf("Error running table: %v", err)
-	}
+	fmt.Printf(tableModel.View())
+	//newTable := tui.NewModel(tableModel, nil, false, false)
+	//err := newTable.Run()
+	//if err != nil {
+	//	con.Log.Errorf("Error running table: %v", err)
+	//}
 }
