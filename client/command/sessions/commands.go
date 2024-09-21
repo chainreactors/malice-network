fbe9084930e529131e1d606d79fa2cc91054d9c9
M09Ic <m09ician@gmail.com>
2024-09-21 15:43:16 +0800

    enhance: remove FormatLongHelp

--- client/command/sessions/commands.go
+++ client/command/sessions/commands.go
@@ -2,7 +2,6 @@ package sessions
 
 import (
 	"github.com/chainreactors/malice-network/client/command/common"
-	"github.com/chainreactors/malice-network/client/command/help"
 	"github.com/chainreactors/malice-network/client/repl"
 	"github.com/chainreactors/malice-network/helper/consts"
 	"github.com/rsteube/carapace"
@@ -14,21 +13,21 @@ func Commands(con *repl.Console) []*cobra.Command {
 	sessionsCmd := &cobra.Command{
 		Use:   consts.CommandSessions,
 		Short: "List sessions",
-		Long: help.FormatLongHelp(`Display a table of active sessions on the server, 
+		Long: `Display a table of active sessions on the server, 
 allowing you to navigate up and down to select a desired session. 
 Press the Enter key to use the selected session. 
 Use the -a or --all option to display all sessions, including those that have been disconnected.
-		`),
+		`,
 		Run: func(cmd *cobra.Command, args []string) {
 			SessionsCmd(cmd, con)
 		},
-		Example: help.FormatLongHelp(`~~~
+		Example: `~~~
 // List all active sessions
 sessions
 
 // List all sessions, including those that have been disconnected
 sessions -a
-			~~~`),
+			~~~`,
 	}
 
 	common.Bind("sessions", true, sessionsCmd, func(f *pflag.FlagSet) {
@@ -38,20 +37,20 @@ sessions -a
 	noteCommand := &cobra.Command{
 		Use:   consts.CommandNote + " [note] [session]",
 		Short: "add note to session",
-		Long: help.FormatLongHelp(`Add a note to a session. If a note already exists, it will be updated. 
-When using an active session, only provide the new note.`),
+		Long: `Add a note to a session. If a note already exists, it will be updated. 
+When using an active session, only provide the new note.`,
 		Args: cobra.MaximumNArgs(2),
 		Run: func(cmd *cobra.Command, args []string) {
 			noteCmd(cmd, con)
 			return
 		},
-		Example: help.FormatLongHelp(`~~~
+		Example: `~~~
 // Add a note to specified session
 note newNote 08d6c05a21512a79a1dfeb9d2a8f262f
 
 // Add a note when using an active session
 note newNote
-			~~~`),
+			~~~`,
 	}
 
 	common.BindArgCompletions(noteCommand,
@@ -63,20 +62,20 @@ note newNote
 	groupCommand := &cobra.Command{
 		Use:   consts.CommandGroup + " [group] [session]",
 		Short: "group session",
-		Long: help.FormatLongHelp(`Add a session to a group. If the group does not exist, it will be created.
-When using an active session, only provide the group name.`),
+		Long: `Add a session to a group. If the group does not exist, it will be created.
+When using an active session, only provide the group name.`,
 		Args: cobra.MaximumNArgs(2),
 		Run: func(cmd *cobra.Command, args []string) {
 			groupCmd(cmd, con)
 			return
 		},
-		Example: help.FormatLongHelp(`~~~
+		Example: `~~~
 // Add a session to a group
 group newGroup 08d6c05a21512a79a1dfeb9d2a8f262f
 
 // Add a session to a group when using an active session
 group newGroup
-			~~~`),
+			~~~`,
 	}
 
 	common.BindArgCompletions(groupCommand,
@@ -88,15 +87,15 @@ group newGroup
 	removeCommand := &cobra.Command{
 		Use:   consts.CommandDelSession + " [session]",
 		Short: "del session",
-		Long:  help.FormatLongHelp("Del a specified session."),
+		Long:  "Del a specified session.",
 		Args:  cobra.MinimumNArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
 			removeCmd(cmd, con)
 		},
-		Example: help.FormatLongHelp(`~~~
+		Example: `~~~
 // Delete a specified session
 del 08d6c05a21512a79a1dfeb9d2a8f262f
-			~~~`),
+			~~~`,
 	}
 
 	common.BindArgCompletions(removeCommand, nil, common.SessionIDCompleter(con))
@@ -104,7 +103,7 @@ del 08d6c05a21512a79a1dfeb9d2a8f262f
 	useCommand := &cobra.Command{
 		Use:   consts.CommandUse + " [session]",
 		Short: "Use session",
-		Long:  help.FormatLongHelp("use"),
+		Long:  "use",
 		Args:  cobra.MinimumNArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
 			UseSessionCmd(cmd, con)
@@ -117,7 +116,7 @@ del 08d6c05a21512a79a1dfeb9d2a8f262f
 	backCommand := &cobra.Command{
 		Use:   consts.CommandBackground,
 		Short: "back to root context",
-		Long:  help.FormatLongHelp("Exit the current session and return to the root context."),
+		Long:  "Exit the current session and return to the root context.",
 		Run: func(cmd *cobra.Command, args []string) {
 			con.ActiveTarget.Background()
 			con.App.SwitchMenu(consts.ClientMenu)
@@ -128,17 +127,17 @@ del 08d6c05a21512a79a1dfeb9d2a8f262f
 	observeCmd := &cobra.Command{
 		Use:   consts.CommandObverse,
 		Short: "observe manager",
-		Long:  help.FormatLongHelp("Control observers to listen session in the background."),
+		Long:  "Control observers to listen session in the background.",
 		Run: func(cmd *cobra.Command, args []string) {
 			ObserveCmd(cmd, con)
 		},
-		Example: help.FormatLongHelp(`~~~
+		Example: `~~~
 // List all observers
 observe -l
 
 // Remove observer
 observe -r
-			~~~`),
+			~~~`,
 	}
 
 	common.BindFlag(observeCmd, func(f *pflag.FlagSet) {
