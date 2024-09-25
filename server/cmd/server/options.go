17fc5914c44ae6c9b0f7cf70cea63a37d45271aa
M09Ic <m09ician@gmail.com>
2024-09-25 15:21:23 +0800

    fix: rootrpc error when ip set

--- server/cmd/server/options.go
+++ server/cmd/server/options.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"github.com/chainreactors/malice-network/helper/utils/mtls"
 	"github.com/chainreactors/malice-network/proto/client/rootpb"
 	"github.com/chainreactors/malice-network/server/internal/configs"
@@ -43,7 +44,7 @@ func (opt *Options) Execute(args []string, parser *flags.Parser) error {
 		return nil
 	}
 	var err error
-	opt.localRpc, err = root.NewRootClient(opt.Server.Address())
+	opt.localRpc, err = root.NewRootClient(fmt.Sprintf("127.0.0.1:%d", opt.Server.GRPCPort))
 	if err != nil {
 		return err
 	}
@@ -78,7 +79,7 @@ func (opt *Options) InitUser() error {
 		return nil
 	}
 
-	client, err := root.NewRootClient(opt.Server.Address())
+	client, err := root.NewRootClient(fmt.Sprintf("127.0.0.1:%d", opt.Server.GRPCPort))
 	if err != nil {
 		return err
 	}
@@ -100,7 +101,7 @@ func (opt *Options) InitListener() error {
 		return nil
 	}
 
-	client, err := root.NewRootClient(opt.Server.Address())
+	client, err := root.NewRootClient(fmt.Sprintf("127.0.0.1:%d", opt.Server.GRPCPort))
 	if err != nil {
 		return err
 	}
