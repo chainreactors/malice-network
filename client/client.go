bdf123ff23ddd4696f8782814ace6f47a42bc057
M09Ic <m09ician@gmail.com>
2024-10-19 23:04:19 +0800

    chore: adapt module proto

--- client/client.go
+++ client/client.go
@@ -3,6 +3,7 @@ package main
 //go:generate protoc -I ../proto/ ../proto/client/clientpb/client.proto --go_out=paths=source_relative:../helper/proto/
 //go:generate protoc -I ../proto/ ../proto/client/rootpb/root.proto --go_out=paths=source_relative:../helper/proto/
 //go:generate protoc -I ../proto/ ../proto/implant/implantpb/implant.proto --go_out=paths=source_relative:../helper/proto/
+//go:generate protoc -I ../proto/ ../proto/implant/implantpb/module.proto --go_out=paths=source_relative:../helper/proto/
 //go:generate protoc -I ../proto/ ../proto/listener/lispb/listener.proto --go_out=paths=source_relative:../helper/proto/
 //go:generate protoc -I ../proto/ ../proto/services/clientrpc/service.proto --go_out=paths=source_relative:../helper/proto/ --go-grpc_out=paths=source_relative:../helper/proto/
 //go:generate protoc -I ../proto/ ../proto/services/listenerrpc/service.proto --go_out=paths=source_relative:../helper/proto/ --go-grpc_out=paths=source_relative:../helper/proto/
