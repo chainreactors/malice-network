afe8f501181024eee5e9a2ffd84d8d3ac197b5d0
M09Ic <m09ician@gmail.com>
2024-11-04 16:38:24 +0800

    enhance: client grpc  reconnect auto reinit

--- client/repl/utils.go
+++ client/repl/utils.go
@@ -3,10 +3,7 @@ package repl
 import (
 	"fmt"
 	"github.com/chainreactors/logs"
-	"github.com/chainreactors/malice-network/client/assets"
-	"github.com/chainreactors/malice-network/client/core"
 	"github.com/chainreactors/malice-network/helper/consts"
-	"github.com/chainreactors/malice-network/helper/utils/mtls"
 	"github.com/chainreactors/tui"
 	"github.com/charmbracelet/lipgloss"
 	"github.com/mattn/go-tty"
@@ -88,50 +85,6 @@ func GetCmd(cmd *cobra.Command, name string) *cobra.Command {
 	}
 	return nil
 
-}
-
-func Login(con *Console, config *mtls.ClientConfig) error {
-	conn, err := mtls.Connect(config)
-	if err != nil {
-		logs.Log.Errorf("Failed to connect: %v", err)
-		return err
-	}
-	logs.Log.Importantf("Connected to server %s", config.Address())
-	con.ServerStatus, err = core.InitServerStatus(conn, config)
-	if err != nil {
-		logs.Log.Errorf("init server failed : %v", err)
-		return err
-	}
-	go con.EventHandler()
-	var pipelineCount = 0
-	for _, i := range con.Listeners {
-		pipelineCount = pipelineCount + len(i.Pipelines.Pipelines)
-	}
-	var alive = 0
-	for _, i := range con.Sessions {
-		if i.IsAlive {
-			alive++
-		}
-	}
-	logs.Log.Importantf("%d listeners, %d pipelines, %d clients, %d sessions (%d alive)",
-		len(con.Listeners), pipelineCount, len(con.Clients), len(con.Sessions), alive)
-	return nil
-}
-
-func NewConfigLogin(con *Console, yamlFile string) error {
-	config, err := mtls.ReadConfig(yamlFile)
-	if err != nil {
-		return err
-	}
-	err = Login(con, config)
-	if err != nil {
-		return err
-	}
-	err = assets.MvConfig(yamlFile)
-	if err != nil {
-		return err
-	}
-	return nil
 }
 
 func AdaptSessionColor(prePrompt, sId string) string {
