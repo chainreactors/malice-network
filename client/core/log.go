6bc5d036e075a4c43a21ff8caca5ad3ccbb286a9
M09Ic <m09ician@gmail.com>
2024-11-11 22:00:01 +0800

    fix: client new line not work

--- client/core/log.go
+++ client/core/log.go
@@ -14,7 +14,7 @@ var (
 )
 
 var (
-	NewLine                    = "\n"
+	NewLine                    = "\x1b[1E"
 	Debug           logs.Level = 10
 	Warn            logs.Level = 20
 	Info            logs.Level = 30
@@ -23,11 +23,11 @@ var (
 	GroupStyle                 = lipgloss.NewStyle().Foreground(lipgloss.Color("#8BE9FD"))
 	NameStyle                  = lipgloss.NewStyle().Foreground(lipgloss.Color("#FF79C6"))
 	DefaultLogStyle            = map[logs.Level]string{
-		Debug:     NewLine + termenv.String(tui.Rocket+"[+]").Bold().Background(tui.Blue).String() + " %s ",
-		Warn:      NewLine + termenv.String(tui.Zap+"[warn]").Bold().Background(tui.Yellow).String() + " %s ",
-		Important: NewLine + termenv.String(tui.Fire+"[*]").Bold().Background(tui.Purple).String() + " %s ",
-		Info:      NewLine + termenv.String(tui.HotSpring+"[i]").Bold().Background(tui.Green).String() + " %s ",
-		Error:     NewLine + termenv.String(tui.Monster+"[-]").Bold().Background(tui.Red).String() + " %s ",
+		Debug:     NewLine + termenv.String(tui.Rocket+"[+]").Bold().Background(tui.Blue).String() + " %s \n",
+		Warn:      NewLine + termenv.String(tui.Zap+"[warn]").Bold().Background(tui.Yellow).String() + " %s \n",
+		Important: NewLine + termenv.String(tui.Fire+"[*]").Bold().Background(tui.Purple).String() + " %s \n",
+		Info:      NewLine + termenv.String(tui.HotSpring+"[i]").Bold().Background(tui.Green).String() + " %s \n",
+		Error:     NewLine + termenv.String(tui.Monster+"[-]").Bold().Background(tui.Red).String() + " %s \n",
 	}
 )
 
