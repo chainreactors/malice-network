fb4e3da890a12722347cc832f6118b30032e5c4b
M09Ic <m09ician@gmail.com>
2024-11-12 18:35:46 +0800

    enhance: union priv type

--- client/command/file/upload.go
+++ client/command/file/upload.go
@@ -9,6 +9,7 @@ import (
 	"github.com/chainreactors/malice-network/helper/proto/services/clientrpc"
 	"github.com/spf13/cobra"
 	"path/filepath"
+	"strconv"
 
 	"os"
 )
@@ -16,7 +17,7 @@ import (
 func UploadCmd(cmd *cobra.Command, con *repl.Console) error {
 	path := cmd.Flags().Arg(0)
 	target := cmd.Flags().Arg(1)
-	priv, _ := cmd.Flags().GetInt("priv")
+	priv, _ := cmd.Flags().GetString("priv")
 	hidden, _ := cmd.Flags().GetBool("hidden")
 
 	task, err := Upload(con.Rpc, con.GetInteractive(), path, target, priv, hidden)
@@ -28,16 +29,19 @@ func UploadCmd(cmd *cobra.Command, con *repl.Console) error {
 	return nil
 }
 
-func Upload(rpc clientrpc.MaliceRPCClient, session *core.Session, path string, target string, priv int, hidden bool) (*clientpb.Task, error) {
+func Upload(rpc clientrpc.MaliceRPCClient, session *core.Session, path string, target string, priv string, hidden bool) (*clientpb.Task, error) {
 	data, err := os.ReadFile(path)
 	if err != nil {
 		core.Log.Errorf("Can't open file: %s", err)
 	}
-
+	value, err := strconv.ParseUint(priv, 8, 32)
+	if err != nil {
+		return nil, err
+	}
 	task, err := rpc.Upload(session.Context(), &implantpb.UploadRequest{
 		Name:   filepath.Base(path),
 		Target: target,
-		Priv:   uint32(priv),
+		Priv:   uint32(value),
 		Data:   data,
 		Hidden: hidden,
 	})
