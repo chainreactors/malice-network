825d09ece681fe54446ad183687d86e33c2e003e
HuYlllc <632781087@qq.com>
2024-11-20 16:36:50 +0800

    feat: add install-cn

--- install-cn.sh
+++ install-cn.sh
@@ -26,43 +26,125 @@ download_file() {
 # check and install docker
 check_install_docker(){
     yum_install_docker(){
-        yum install -y yum-utils curl unzip git
-        yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+        yum install -y yum-utils curl unzip git 
+        yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
         yum makecache fast
         yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
     }
     apt_install_docker(){
         apt update && apt install -y ca-certificates curl unzip git
         install -m 0755 -d /etc/apt/keyrings
-        curl -fsSL "https://download.docker.com/linux/$ID/gpg" -o /etc/apt/keyrings/docker.asc
+        curl -fsSL "https://mirrors.aliyun.com/docker-ce/linux/$ID/gpg" -o /etc/apt/keyrings/docker.asc
         chmod a+r /etc/apt/keyrings/docker.asc
-        echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/$ID $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | tee /etc/apt/sources.list.d/docker.list
+        echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://mirrors.aliyun.com/docker-ce/linux/$ID $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | tee /etc/apt/sources.list.d/docker.list
         apt update -y && apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
     }
+    change_docker_daemon(){
+        config_file="/etc/docker/daemon.json"
+        mkdir -p $(dirname "$config_file")
+
+        # 默认的镜像列表
+        default_mirrors=(
+            "https://mirror.ccs.tencentyun.com" # 腾讯云
+            "https://dockerhub.azk8s.cn"        # Azure 中国
+            "https://docker.fxxk.dedyn.io"
+            "https://docker.1ms.run"
+            "https://dockerpull.org"
+            "https://dockerhub.timeweb.cloud" 
+        )
+
+        # 提示用户输入自定义镜像地址
+        echo "==========================================================="
+        echo "可以根据当前的服务器厂商添加Docker 镜像加速源，以加速 Docker 镜像的拉取。"
+        echo "以下是一些常见的云厂商内部镜像地址供参考："
+        echo "  1. 腾讯云服务器可用: https://mirror.ccs.tencentyun.com"
+        echo "  2. Azure 可用: https://dockerhub.azk8s.cn"
+        echo "  3. 阿里云可用: https://<your_code>.mirror.aliyuncs.com"
+        echo "     （登录阿里云查看 https://cr.console.aliyun.com/cn-hongkong/instances/mirrors）"
+        echo "  4. 华为云可用: https://<your_code>.mirror.swr.myhuaweicloud.com"
+        echo "     （登录华为云查看https://console.huaweicloud.com/swr/?region=cn-north-4#/swr/mirror）"
+        echo "-----------------------------------------------------------"
+        echo "请输入自定义的 Docker 镜像加速源（每行一个如http://aa.bb.cc, 输入完成后按 Enter 跳过）："
+        echo "==========================================================="
+
+        user_mirrors=()
+        while read -e -p "自定义镜像地址 (按 Enter 跳过或结束输入): " line; do
+            if [ -z "$line" ]; then
+                break
+            fi
+            if echo "$line" | grep -Eq "^https?://[a-zA-Z0-9.-]+(/[a-zA-Z0-9._~:/?\#\[\]@!\$&\'\(\)\*\+,;=-]*)?$"; then
+                if [[ " ${default_mirrors[*]} " =~ " $line " ]]; then
+                    echo "输入的地址 '$line' 已存在于默认镜像中，跳过添加。"
+                else
+                    user_mirrors+=("\"$line\"")
+                fi
+            else
+                echo "输入的地址 '$line' 格式无效，请确保以 'http://' 或 'https://' 开头并符合 URL 格式。"
+            fi
+        done
+        # 组合用户输入和默认值
+        combined_mirrors=("${user_mirrors[@]}")
+        for mirror in "${default_mirrors[@]}"; do
+            combined_mirrors+=("\"$mirror\"")
+        done
+
+        # 备份现有配置文件
+        if [ -f "$config_file" ]; then
+            echo "备份现有配置文件到 $config_file.bak"
+            mv "$config_file" "$config_file.bak"
+        fi
+
+        # 写入新配置文件
+        echo "正在生成新的 Docker 配置文件..."
+        {
+            echo "{"
+            echo "  \"registry-mirrors\": ["
+            echo "    $(IFS=,; echo "${combined_mirrors[*]}")"
+            echo "  ]"
+            echo "}"
+        } > "$config_file"
+
+        # 重启 Docker 服务
+        systemctl daemon-reload
+        systemctl restart docker
+
+        echo "Docker 配置已更新, 你可到 $config_file 查看配置内容。"
+    }
+
     if [ -f /etc/os-release ]; then
         . /etc/os-release
     else
-        log_task_status ended "Unsupported OS"
+        log_task_status ended "当前操作系统不支持"
         exit 1
     fi
     if ! command -v docker &> /dev/null; then
-        log_task_status in_progress "Docker is not installed, installing..."
+        log_task_status in_progress "检测到Docker 未安装，正在安装..."
         if [ "$ID" = "centos" ] ; then
             yum_install_docker
         elif [ "$ID" = "ubuntu" ] || [ "$ID" = "debian" ]; then
             apt_install_docker
         else
-            log_task_status ended "Unsupported OS"
+            log_task_status ended "当前操作系统不支持"
             exit 1
         fi
     else
-        log_task_status completed "Docker is already installed, Skipping .." 
+        log_task_status completed "Docker 已安装，跳过安装..."
     fi
-    log_task_status completed "Docker is installed, Docker version: $(docker --version)"
+    
+    echo "是否需要配置 Docker 镜像加速源？[y/n]"
+    read -r user_choice
+    if [[ "$user_choice" == "y" || "$user_choice" == "Y" || "$user_choice" == "yes" || "$user_choice" == "YES" ]]; then
+        change_docker_daemon
+    else
+        log_task_status in_progress "操作已取消，Docker 镜像加速源保持不变。"
+    fi
+    
+    log_task_status completed "Docker 安装完成，当前 Docker 版本为：$(docker --version)"
 }
+
 # pull images for compilation
 docker_pull_image(){
-    SOURCE_NAME_SPACE=${SOURCE_NAME_SPACE:="ghcr.io/chainreactors"}
+    SOURCE_NAME_SPACE=${SOURCE_NAME_SPACE:="chainreactors"}
     FINAL_NAME_SPACE=${FINAL_NAME_SPACE:="ghcr.io/chainreactors"}
     # if you want more images, add them to the array
     images=(
@@ -87,29 +169,29 @@ docker_pull_image(){
 # set your server ip
 
 setup_environment(){
-  set_server_ip(){
-      default_ip=$(curl --noproxy -4 -s ifconfig.me)
-      if [[ -t 0 ]]; then
-          read -p "Please input your IP Address for the server to start [default: $default_ip]: " input_ip
-          ip_address=${input_ip:-$default_ip}
-      else
-          ip_address=$default_ip
-          log_task_status "completed" "No interactive shell detected. Using default IP Address: $ip_address"
-      fi
-      log_task_status completed "Using IP Address: $ip_address"
-  }
+    set_server_ip(){
+        default_ip=$(curl --noproxy -4 -s ifconfig.me)
+        if [[ -t 0 ]]; then
+            read -p "Please input your IP Address for the server to start [default: $default_ip]: " input_ip
+            ip_address=${input_ip:-$default_ip}
+        else
+            ip_address=$default_ip
+            log_task_status "completed" "No interactive shell detected. Using default IP Address: $ip_address"
+        fi
+        log_task_status completed "Using IP Address: $ip_address"
+    }
 
-  set_base_dir(){
-      local DEFAULT_DIR="/opt/iom"
-      if [[ -t 0 ]]; then
-          read -p "Please input the base directory for the installation [default: $DEFAULT_DIR]: " input_dir
-          IoM_ROOT_DIR=${input_dir:-$DEFAULT_DIR}
-      else
-          IoM_ROOT_DIR=$DEFAULT_DIR
-          log_task_status "completed" "No interactive shell detected. Using default base directory: $IoM_ROOT_DIR"
-      fi
-      log_task_status completed "Using base directory: $IoM_ROOT_DIR"
-  }
+    set_base_dir(){
+        local DEFAULT_DIR="/opt/iom"
+        if [[ -t 0 ]]; then
+            read -p "Please input the base directory for the installation [default: $DEFAULT_DIR]: " input_dir
+            IoM_ROOT_DIR=${input_dir:-$DEFAULT_DIR}
+        else
+            IoM_ROOT_DIR=$DEFAULT_DIR
+            log_task_status "completed" "No interactive shell detected. Using default base directory: $IoM_ROOT_DIR"
+        fi
+        log_task_status completed "Using base directory: $IoM_ROOT_DIR"
+    }
     set_base_dir
     set_server_ip
 }
