c8dbb34b1ed4a78e95e3f223f64c79e42a67cf29
M09Ic <m09ician@gmail.com>
2024-12-10 13:12:01 +0800

    feat: add self_stager and self_payload

--- client/command/build/artifact.go
+++ client/command/build/artifact.go
@@ -163,12 +163,13 @@ func UploadArtifact(con *repl.Console, path string, name, artifactType, stage st
 	})
 }
 
-func SearchArtifact(con *repl.Console, os, arch, typ, pipeline string) (*clientpb.Artifact, error) {
+func SearchArtifact(con *repl.Console, os, arch, typ, pipeline string, isSRDI bool) (*clientpb.Artifact, error) {
 	return con.Rpc.FindArtifact(con.Context(), &clientpb.Artifact{
 		Arch:     arch,
 		Platform: os,
 		Type:     typ,
 		Pipeline: pipeline,
+		IsSrdi:   isSRDI,
 	})
 }
 
