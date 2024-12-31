334c47d253e6c759c928dacce1ca02fbe1186336
h3zh1 <h3zh1@qq.com>
2024-12-31 14:35:45 +0800

    refactor: replace releaseLto with release

--- server/internal/build/config.go
+++ server/internal/build/config.go
@@ -38,21 +38,21 @@ func MoveBuildOutput(target, buildType string) (string, string, error) {
 	switch {
 	case strings.Contains(target, "windows"):
 		if buildType == consts.CommandBuildModules {
-			sourcePath = filepath.Join(configs.TargetPath, target, releaseLto, modules+consts.DllFile)
+			sourcePath = filepath.Join(configs.TargetPath, target, release, modules+consts.DllFile)
 		} else if buildType == consts.CommandBuildPrelude {
-			sourcePath = filepath.Join(configs.TargetPath, target, releaseLto, prelude+consts.PEFile)
+			sourcePath = filepath.Join(configs.TargetPath, target, release, prelude+consts.PEFile)
 		} else if buildType == consts.CommandBuildPulse {
-			sourcePath = filepath.Join(configs.TargetPath, target, releaseLto, pulse+consts.PEFile)
+			sourcePath = filepath.Join(configs.TargetPath, target, release, pulse+consts.PEFile)
 		} else {
-			sourcePath = filepath.Join(configs.TargetPath, target, releaseLto, malefic+consts.PEFile)
+			sourcePath = filepath.Join(configs.TargetPath, target, release, malefic+consts.PEFile)
 		}
 	case strings.Contains(target, "darwin"):
-		sourcePath = filepath.Join(configs.TargetPath, target, releaseLto, malefic)
+		sourcePath = filepath.Join(configs.TargetPath, target, release, malefic)
 	case strings.Contains(target, "linux"):
 		if buildType == consts.CommandBuildPrelude {
-			sourcePath = filepath.Join(configs.TargetPath, target, releaseLto, prelude)
+			sourcePath = filepath.Join(configs.TargetPath, target, release, prelude)
 		} else {
-			sourcePath = filepath.Join(configs.TargetPath, target, releaseLto, malefic)
+			sourcePath = filepath.Join(configs.TargetPath, target, release, malefic)
 		}
 	}
 	dstPath := filepath.Join(configs.BuildOutputPath, name)
