53edb5e4936580175727c4af850a5979f0ad05e4
M09Ic <m09ician@gmail.com>
2025-01-10 16:59:09 +0800

    fix: reg add command

--- client/command/reg/add.go
+++ client/command/reg/add.go
@@ -1,7 +1,11 @@
 package reg
 
 import (
+	"encoding/hex"
 	"fmt"
+	"strconv"
+	"strings"
+
 	"github.com/chainreactors/malice-network/client/command/common"
 	"github.com/chainreactors/malice-network/client/core"
 	"github.com/chainreactors/malice-network/client/repl"
@@ -18,34 +22,57 @@ func RegAddCmd(cmd *cobra.Command, con *repl.Console) error {
 	// 解析注册表的各项参数
 	path := cmd.Flags().Arg(0)
 	hive, path := FormatRegPath(path)
-	key := cmd.Flags().Arg(1)
-	stringValue, _ := cmd.Flags().GetString("string_value")
-	byteValue, _ := cmd.Flags().GetBytesBase64("byte_value")
-	dwordValue, _ := cmd.Flags().GetUint32("dword_value")
-	qwordValue, _ := cmd.Flags().GetUint64("qword_value")
-	regtype, _ := cmd.Flags().GetUint32("regtype")
+
+	// 获取参数
+	valueName, _ := cmd.Flags().GetString("v")
+	valueType, _ := cmd.Flags().GetString("t")
+	data, _ := cmd.Flags().GetString("d")
 
 	session := con.GetInteractive()
-	task, err := RegAdd(con.Rpc, session, hive, path, key, stringValue, byteValue, dwordValue, qwordValue, regtype)
+	task, err := RegAdd(con.Rpc, session, hive, path, valueName, valueType, data)
 	if err != nil {
 		return err
 	}
 
-	session.Console(task, fmt.Sprintf("add or modify registry key: %s\\%s\\%s", hive, path, key))
+	session.Console(task, fmt.Sprintf("add or modify registry key: %s\\%s\\%s", hive, path, valueName))
 	return nil
 }
 
-func RegAdd(rpc clientrpc.MaliceRPCClient, session *core.Session, hive, path, key, stringValue string, byteValue []byte, dwordValue uint32, qwordValue uint64, regtype uint32) (*clientpb.Task, error) {
+func RegAdd(rpc clientrpc.MaliceRPCClient, session *core.Session, hive, path string, valueName, valueType, data string) (*clientpb.Task, error) {
 	request := &implantpb.RegistryWriteRequest{
-		Hive:        hive,
-		Path:        fileutils.FormatWindowPath(path),
-		Key:         key,
-		StringValue: stringValue,
-		ByteValue:   byteValue,
-		DwordValue:  dwordValue,
-		QwordValue:  qwordValue,
-		Regtype:     regtype,
+		Hive: hive,
+		Path: fileutils.FormatWindowPath(path),
+		Key:  valueName,
 	}
+
+	// 根据类型转换数据
+	switch valueType {
+	case "REG_SZ":
+		request.StringValue = data
+		request.Regtype = 1
+	case "REG_BINARY":
+		// 将十六进制字符串转换为字节数组
+		byteValue, _ := hex.DecodeString(strings.ReplaceAll(data, " ", ""))
+		request.ByteValue = byteValue
+		request.Regtype = 3
+	case "REG_DWORD":
+		// 将字符串转换为 uint32
+		if v, err := strconv.ParseUint(data, 0, 32); err == nil {
+			request.DwordValue = uint32(v)
+		}
+		request.Regtype = 4
+	case "REG_QWORD":
+		// 将字符串转换为 uint64
+		if v, err := strconv.ParseUint(data, 0, 64); err == nil {
+			request.QwordValue = v
+		}
+		request.Regtype = 11
+	default:
+		// 默认使用 REG_SZ
+		request.StringValue = data
+		request.Regtype = 1
+	}
+
 	return rpc.RegAdd(session.Context(), request)
 }
 
@@ -61,17 +88,14 @@ func RegisterRegAddFunc(con *repl.Console) {
 	con.AddCommandFuncHelper(
 		consts.ModuleRegAdd,
 		consts.ModuleRegAdd,
-		consts.ModuleRegAdd+"(active(),\"HKEY_LOCAL_MACHINE\",\"SOFTWARE\\Example\",\"TestKey\",\"example\",\"\",1,0,0)",
+		consts.ModuleRegAdd+"(active(),\"HKEY_LOCAL_MACHINE\",\"SOFTWARE\\Example\",\"TestValue\",\"REG_DWORD\",\"1\")",
 		[]string{
 			"session: special session",
 			"hive: registry hive",
 			"path: registry path",
-			"key: registry",
-			"stringValue: string value",
-			"byteValue: byte value",
-			"dwordValue: dword value",
-			"qwordValue: qword value",
-			"regtype: registry type",
+			"valueName: value name",
+			"valueType: value type (REG_SZ, REG_BINARY, REG_DWORD, REG_QWORD)",
+			"data: value data",
 		},
 		[]string{"task"})
 }
