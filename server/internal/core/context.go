1d104aceb62700701b9c6445745041afe68af746
HuYlllc <632781087@qq.com>
2025-01-20 23:25:00 +0800

    refactor: refactor core context

--- server/internal/core/context.go
+++ server/internal/core/context.go
@@ -3,7 +3,9 @@ package core
 import (
 	"github.com/chainreactors/logs"
 	"github.com/chainreactors/malice-network/helper/consts"
+	"github.com/chainreactors/malice-network/helper/errs"
 	"github.com/chainreactors/malice-network/helper/proto/client/clientpb"
+	"github.com/chainreactors/malice-network/helper/types"
 	"github.com/chainreactors/malice-network/server/internal/db"
 	"github.com/chainreactors/malice-network/server/internal/db/models"
 	"sync"
@@ -11,34 +13,98 @@ import (
 
 type ContextsResp []*clientpb.Context
 
-type Context struct {
+type contexts struct {
 	sync.Map
 }
 
-var Contexts *Context
+var Contexts *contexts
 
-func NewContext() *Context {
-	newContexts := &Context{
+type Context struct {
+	ID       string
+	Session  *Session
+	Pipeline *clientpb.Pipeline
+	Listener *Listener
+	Task     *Task
+
+	Type  string
+	Value types.Context
+}
+
+func (c *Context) ToProtobuf() *clientpb.Context {
+	resp := &clientpb.Context{
+		Session:  c.Session.ToProtobuf(),
+		Listener: c.Listener.ToProtobuf(),
+		Pipeline: c.Pipeline,
+		Task:     c.Task.ToProtobuf(),
+		Type:     c.Type,
+		Value:    c.Value.String(),
+	}
+	return resp
+}
+
+func NewContext(ctx *clientpb.Context) (*Context, error) {
+	session, ok := Sessions.Get(ctx.Session.SessionId)
+	if !ok {
+		return nil, errs.ErrNotFoundSession
+	}
+	listener, err := Listeners.Get(ctx.Listener.Id)
+	if err != nil {
+		return nil, err
+	}
+	pipeline := listener.Pipelines[ctx.Pipeline.Name]
+	task := session.Tasks.Get(ctx.Task.TaskId)
+	var value types.Context
+	if ctx.Type == consts.ScreenShotType {
+		value, err = types.NewScreenShot([]byte(ctx.Value))
+		if err != nil {
+			return nil, err
+		}
+	} else if ctx.Type == consts.CredentialType {
+		value, err = types.NewCredential([]byte(ctx.Value))
+		if err != nil {
+			return nil, err
+		}
+	} else if ctx.Type == consts.KeyLoggerType {
+		value, err = types.NewKeyLogger([]byte(ctx.Value))
+		if err != nil {
+			return nil, err
+		}
+	}
+	context := &Context{
+		ID:       ctx.Id,
+		Session:  session,
+		Pipeline: pipeline,
+		Listener: listener,
+		Task:     task,
+		Type:     ctx.Type,
+		Value:    value,
+	}
+	return context, nil
+}
+
+func NewContexts() *contexts {
+	newContexts := &contexts{
 		Map: sync.Map{},
 	}
 	Contexts = newContexts
 	return newContexts
 }
 
-func (ctx *Context) WithSession(sid string) ContextsResp {
+func (ctx *contexts) WithSession(sid string) ContextsResp {
 	var contexts ContextsResp
 
 	ctx.Map.Range(func(key, value interface{}) bool {
-		if c, ok := value.(*models.Context); ok {
-			c.SessionID = sid
-			contexts = append(contexts, c.ToProtobuf())
+		if c, ok := value.(*Context); ok {
+			if c.Session.ID == sid {
+				contexts = append(contexts, c.ToProtobuf())
+			}
 		}
 		return true
 	})
 	return contexts
 }
 
-func (ctx *Context) WithPipeline(pid string) ContextsResp {
+func (ctx *contexts) WithPipeline(pid string) ContextsResp {
 	var contexts ContextsResp
 
 	ctx.Map.Range(func(key, value interface{}) bool {
@@ -51,7 +117,7 @@ func (ctx *Context) WithPipeline(pid string) ContextsResp {
 	return contexts
 }
 
-func (ctx *Context) WithListener(lName string) ContextsResp {
+func (ctx *contexts) WithListener(lName string) ContextsResp {
 	var contexts ContextsResp
 
 	ctx.Map.Range(func(key, value interface{}) bool {
@@ -65,7 +131,7 @@ func (ctx *Context) WithListener(lName string) ContextsResp {
 
 }
 
-func (ctx *Context) WithTask(tName string) ContextsResp {
+func (ctx *contexts) WithTask(tName string) ContextsResp {
 	var contexts ContextsResp
 
 	ctx.Map.Range(func(key, value interface{}) bool {
@@ -79,7 +145,7 @@ func (ctx *Context) WithTask(tName string) ContextsResp {
 
 }
 
-func (ctx *Context) ScreenShot() ContextsResp {
+func (ctx *contexts) ScreenShot() ContextsResp {
 	var contexts ContextsResp
 
 	ctx.Map.Range(func(key, value interface{}) bool {
@@ -93,7 +159,7 @@ func (ctx *Context) ScreenShot() ContextsResp {
 	return contexts
 }
 
-func (ctx *Context) KeyLogger() ContextsResp {
+func (ctx *contexts) KeyLogger() ContextsResp {
 	var contexts ContextsResp
 
 	ctx.Map.Range(func(key, value interface{}) bool {
@@ -107,7 +173,7 @@ func (ctx *Context) KeyLogger() ContextsResp {
 	return contexts
 }
 
-func (ctx *Context) Credential() ContextsResp {
+func (ctx *contexts) Credential() ContextsResp {
 	var contexts ContextsResp
 
 	ctx.Map.Range(func(key, value interface{}) bool {
@@ -121,34 +187,34 @@ func (ctx *Context) Credential() ContextsResp {
 	return contexts
 }
 
-func (ctx *Context) Add(c *models.Context) {
-	ctx.Store(c.ID.String(), c)
+func (ctx *contexts) Add(c *Context) {
+	ctx.Store(c.ID, c)
 }
 
-func (ctx *Context) Remove(cID string) {
+func (ctx *contexts) Remove(cID string) {
 	val, ok := ctx.Load(cID)
 	if !ok {
 		logs.Log.Errorf("Context not found: %s", cID)
 		return
 	}
-	v := val.(*models.Context)
+	v := val.(*Context)
 	ctx.Delete(v.ID)
 
 }
 
-func (ctx *Context) Get(cID string) (*models.Context, bool) {
+func (ctx *contexts) Get(cID string) (*Context, bool) {
 	val, ok := ctx.Load(cID)
 	if !ok {
 		return nil, false
 	}
-	v := val.(*models.Context)
+	v := val.(*Context)
 	return v, true
 }
 
-func (ctx *Context) All() []*models.Context {
-	var contexts []*models.Context
+func (ctx *contexts) All() []*Context {
+	var contexts []*Context
 	ctx.Map.Range(func(key, value interface{}) bool {
-		if c, ok := value.(*models.Context); ok {
+		if c, ok := value.(*Context); ok {
 			contexts = append(contexts, c)
 		}
 		return true
@@ -156,13 +222,18 @@ func (ctx *Context) All() []*models.Context {
 	return contexts
 }
 
-func (ctx *Context) Recover() error {
+func RecoverContext() error {
 	contexts, err := db.GetAllContext()
 	if err != nil {
 		return err
 	}
 	for _, c := range contexts {
-		ctx.Add(c)
+		context := c.ToProtobuf()
+		newContext, err := NewContext(context)
+		if err != nil {
+			return err
+		}
+		Contexts.Add(newContext)
 	}
 	return nil
 }
