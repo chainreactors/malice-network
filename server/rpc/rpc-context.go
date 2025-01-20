a641e654ce6b31d91820e71d1b7148de8958e42c
HuYlllc <632781087@qq.com>
2025-01-21 03:08:48 +0800

    fix: remove context duplicate code

--- server/rpc/rpc-context.go
+++ server/rpc/rpc-context.go
@@ -12,21 +12,31 @@ import (
 
 func (rpc *Server) GetContexts(ctx context.Context, req *clientpb.Context) (*clientpb.Contexts, error) {
 	contexts := make([]*clientpb.Context, 0)
-	for _, c := range core.Contexts.All() {
+
+	filterFunc := func(c *core.Context) bool {
 		if req.Session != nil && req.Session.SessionId != "" && c.Session.ID != req.Session.SessionId {
-			continue
-		} else if req.Task != nil && req.Task.TaskId != 0 && c.Task.TaskID() != req.Task.SessionId+"-"+strconv.Itoa(int(req.Task.TaskId)) {
-			continue
-		} else if req.Pipeline != nil && req.Pipeline.Name != "" && c.Pipeline.Name != req.Pipeline.Name {
-			continue
-		} else if req.Listener != nil && req.Listener.Id != "" && c.Listener.Name != req.Listener.Id {
-			continue
+			return false
 		}
-		contexts = append(contexts, c.ToProtobuf())
+		if req.Task != nil && req.Task.TaskId != 0 && c.Task.TaskID() != req.Task.SessionId+"-"+strconv.Itoa(int(req.Task.TaskId)) {
+			return false
+		}
+		if req.Pipeline != nil && req.Pipeline.Name != "" && c.Pipeline.Name != req.Pipeline.Name {
+			return false
+		}
+		if req.Listener != nil && req.Listener.Id != "" && c.Listener.Name != req.Listener.Id {
+			return false
+		}
+		return true
 	}
+
+	for _, c := range core.Contexts.All() {
+		if filterFunc(c) {
+			contexts = append(contexts, c.ToProtobuf())
+		}
+	}
+
 	return &clientpb.Contexts{Contexts: contexts}, nil
 }
-
 func (rpc *Server) GetScreenShot(ctx context.Context, req *clientpb.Empty) (*clientpb.Contexts, error) {
 	return core.Contexts.ScreenShot(), nil
 }
