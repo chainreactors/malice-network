f68a9de881b12264dc7d7f3ccedbfe947cc5b12e
M09Ic <m09ician@gmail.com>
2025-02-28 03:46:04 +0800

    chore: update context list print

--- client/command/context/context.go
+++ client/command/context/context.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"github.com/chainreactors/malice-network/client/repl"
 	"github.com/chainreactors/malice-network/helper/proto/client/clientpb"
+	"github.com/chainreactors/tui"
+	"github.com/evertras/bubble-table/table"
 	"github.com/spf13/cobra"
 )
 
@@ -13,10 +15,26 @@ func ListContexts(cmd *cobra.Command, con *repl.Console) error {
 		return err
 	}
 
-	// 格式化输出所有contexts
-	for _, ctx := range contexts.GetContexts() {
-		fmt.Printf("[%s] %s\n", ctx.Type, ctx.Value)
+	var rowEntries []table.Row
+	for _, ctx := range contexts.Contexts {
+		row := table.NewRow(table.RowData{
+			"ID":      ctx.Id,
+			"Session": ctx.Session.SessionId,
+			"Task":    getTaskId(ctx.Task),
+			"Type":    ctx.Type,
+		})
+		rowEntries = append(rowEntries, row)
 	}
+
+	tableModel := tui.NewTable([]table.Column{
+		table.NewColumn("ID", "ID", 36),
+		table.NewColumn("Session", "Session", 8),
+		table.NewColumn("Task", "Task", 8),
+		table.NewColumn("Type", "Type", 8),
+	}, true)
+
+	tableModel.SetRows(rowEntries)
+	fmt.Printf(tableModel.View())
 	return nil
 }
 
