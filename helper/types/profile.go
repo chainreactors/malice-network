c27f443379e12fb6a128530e307a9427c2077527
h3zh1 <h3zh1@qq.com>
2025-04-24 23:35:52 +0800

    fix: update build 、profile

--- helper/types/profile.go
+++ helper/types/profile.go
@@ -22,16 +22,17 @@ func LoadProfile(content []byte) (*ProfileConfig, error) {
 }
 
 type BasicProfile struct {
-	Name       string   `yaml:"name" config:"name" default:"malefic"`
-	Targets    []string `yaml:"targets" config:"targets" default:"[]"`
-	Protocol   string   `yaml:"protocol" config:"protocol" default:"tcp"`
-	TLS        bool     `yaml:"tls" config:"tls" default:"false"`
-	Proxy      string   `yaml:"proxy" config:"proxy" default:""`
-	Interval   int      `yaml:"interval" config:"interval" default:"5"`
-	Jitter     float64  `yaml:"jitter" config:"jitter" default:"0.2"`
-	CA         string   `yaml:"ca" config:"ca" default:""`
-	Encryption string   `yaml:"encryption" config:"encryption" default:"aes"`
-	Key        string   `yaml:"key" config:"key" default:"maliceofinternal"`
+	Name       string                 `yaml:"name" config:"name" default:"malefic"`
+	Targets    []string               `yaml:"targets" config:"targets" default:"[]"`
+	Protocol   string                 `yaml:"protocol" config:"protocol" default:"tcp"`
+	TLS        bool                   `yaml:"tls" config:"tls" default:"false"`
+	Proxy      string                 `yaml:"proxy" config:"proxy" default:""`
+	Interval   int                    `yaml:"interval" config:"interval" default:"5"`
+	Jitter     float64                `yaml:"jitter" config:"jitter" default:"0.2"`
+	CA         string                 `yaml:"ca" config:"ca" default:""`
+	Encryption string                 `yaml:"encryption" config:"encryption" default:"aes"`
+	Key        string                 `yaml:"key" config:"key" default:"maliceofinternal"`
+	Extras     map[string]interface{} `yaml:",inline"`
 }
 
 type PulseProfile struct {
