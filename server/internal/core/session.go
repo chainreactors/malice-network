1b480a8ed30e0b38ac07f418616122db6f92ebd1
M09Ic <m09ician@gmail.com>
2025-04-28 15:04:49 +0800

    fix: add session nil check,  https://github.com/chainreactors/malice-network/issues/31

--- server/internal/core/session.go
+++ server/internal/core/session.go
@@ -534,6 +534,10 @@ func (s *sessions) Get(sessionID string) (*Session, error) {
 }
 
 func (s *sessions) Add(session *Session) *Session {
+	if session == nil {
+		logs.Log.Errorf("session is nil")
+		return nil
+	}
 	s.active.Store(session.ID, session)
 	//EventBroker.Publish(Event{
 	//	EventType: consts.SessionOpenedEvent,
