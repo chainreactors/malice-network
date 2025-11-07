7e7b07ade22361b1330ea83522580d1329f32264
M09Ic <m09ician@gmail.com>
2025-11-07 16:17:41 +0800

    fix: community embed

--- helper/intl/community.go
+++ helper/intl/community.go
@@ -7,5 +7,8 @@ import (
 	"embed"
 )
 
-//go:embed community/*
+//go:embed community/main.lua
+//go:embed community/mal.yaml
+//go:embed community/modules
+//go:embed community/resources
 var UnifiedFS embed.FS
