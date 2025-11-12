efcc7b635cb1fdaf780befb9dd16c32566e224b9
M09Ic <m09ician@gmail.com>
2025-11-12 22:01:59 +0800

    chore: fix proto generate

--- client/client.go
+++ client/client.go
@@ -1,11 +1,11 @@
 package main
 
-//go:generate protoc -I ../proto/ ../proto/client/clientpb/client.proto --go_out=paths=source_relative:../helper/proto/
-//go:generate protoc -I ../proto/ ../proto/client/rootpb/root.proto --go_out=paths=source_relative:../helper/proto/
-//go:generate protoc -I ../proto/ ../proto/implant/implantpb/implant.proto --go_out=paths=source_relative:../helper/proto/
-//go:generate protoc -I ../proto/ ../proto/implant/implantpb/module.proto --go_out=paths=source_relative:../helper/proto/
-//go:generate protoc -I ../proto/ ../proto/services/clientrpc/service.proto --go_out=paths=source_relative:../helper/proto/ --go-grpc_out=paths=source_relative:../helper/proto/
-//go:generate protoc -I ../proto/ ../proto/services/listenerrpc/service.proto --go_out=paths=source_relative:../helper/proto/ --go-grpc_out=paths=source_relative:../helper/proto/
+//go:generate protoc -I ../proto/ ../proto/client/clientpb/client.proto --go_out=paths=source_relative:../external/IoM-go/proto
+//go:generate protoc -I ../proto/ ../proto/client/rootpb/root.proto --go_out=paths=source_relative:../external/IoM-go/proto
+//go:generate protoc -I ../proto/ ../proto/implant/implantpb/implant.proto --go_out=paths=source_relative:../external/IoM-go/proto
+//go:generate protoc -I ../proto/ ../proto/implant/implantpb/module.proto --go_out=paths=source_relative:../external/IoM-go/proto
+//go:generate protoc -I ../proto/ ../proto/services/clientrpc/service.proto --go_out=paths=source_relative:../external/IoM-go/proto --go-grpc_out=paths=source_relative:../external/IoM-go/proto
+//go:generate protoc -I ../proto/ ../proto/services/listenerrpc/service.proto --go_out=paths=source_relative:../external/IoM-go/proto --go-grpc_out=paths=source_relative:../external/IoM-go/proto
 
 import (
 	"github.com/chainreactors/logs"
